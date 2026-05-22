fe8e4f2587a109b0226aba43435837266725807d
ApurvaDesai6 <91203794+ApurvaDesai6@users.noreply.github.com>
2026-05-22 04:41:15 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  outputFileTracingIncludes: {
+    "/api/**": ["./data/**/*.json"],
+  },
+};
+
+export default nextConfig;
